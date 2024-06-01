c6922d99998ae3392cbc468ddae605b0703d22cd
SOMA VASUDEV <svsoma382@gmail.com>
2024-06-01 15:43:51 +0530

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Third-year B.Tech student studying at University of College Engineering and Technology, MGU. I am intrested in web devlopment.;
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "HTML",
+  "CSS",
+  "Javascript",
+  "PHP",
+  "Bootstrap",
+  "MySQL",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "Passionate Third-year Btech Student with a keen interest in web development, proficient in HTML, CSS, JavaScript, PHP, Bootstrap and Python. Possessing foundational Knowledge of Node.js and React js. Eager to contribute my skills and drive for continuous learning to impactful projects.";
 
 const About = () => {
   return (
